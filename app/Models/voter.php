38d817fff74a932bfcda3235932846cb0f1c5d27
Richard Wilson <richardmwilson191@gmail.com>
2021-10-09 11:11:13 -0500

    Renamed some classes

--- app/Models/voter.php
+++ app/Models/voter.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class voter extends Model
+class Voter extends Model
 {
     use HasFactory;
 
