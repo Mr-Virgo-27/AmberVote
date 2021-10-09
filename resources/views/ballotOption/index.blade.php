fbfdd60f88c3f5273ee9a2ea8ea480489968acb5
Shaneika Lewis <amberstudentneika@gmail.com>
2021-10-09 01:12:39 -0500

    EDit an delete ballot option

--- resources/views/ballotOption/index.blade.php
+++ resources/views/ballotOption/index.blade.php
@@ -27,7 +27,6 @@
         <tbody class="block md:table-row-group">
         @foreach($data as $info)
             <tr class="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
-
                 <td class="p-2 md:border md:border-grey-500 text-center block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold"></span>{{$info->BallotQuestions->Question}}</td>
                 <td class="p-2 md:border md:border-grey-500 text-left  block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold"></span>{{$info->option}}</td>
                 <td class="p-2 md:border md:border-grey-500 text-center  block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold"></span>{{$info->max_res}}</td>
