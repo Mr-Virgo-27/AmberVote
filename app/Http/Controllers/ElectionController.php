3716e04d986c3c150ac44c25d961095163ce25d5
Shaneika Lewis <amberstudentneika@gmail.com>
2021-10-10 13:10:31 -0500

    commit

--- app/Http/Controllers/ElectionController.php
+++ app/Http/Controllers/ElectionController.php
@@ -4,29 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\Election;
 use Illuminate\Http\Request;
+use Auth;
 
 class ElectionController extends Controller
 {
-    public function ElectionIndex()
+    public function electionIndex()
     {
         $elections = Election::where('user_id', auth()->user()->id)->get();
         return view('election.index', compact('elections'));
     }
 
-    public function AddElection()
+    public function addElection()
     {
         return view('election.create');
     }
 
-    public function ElectionStore(Request $req)
+    public function electionStore(Request $req)
     {
         Election::create([
+            'user_id' => Auth::User()->id,
             'election_nm' => $req->election_nm,
             'start_date' => $req->start,
             'end_date' => $req->end,
             'desc' => $req->desc
         ]);
 
-        return redirect()->route('Election.Index');
+        return redirect()->route('election.Index');
     }
 }
--- routes/lewis.php
+++ routes/lewis.php
@@ -1,14 +1,14 @@
 <?php
 
-Route::get('/BallotQuestion', [\App\Http\Controllers\BallotQuestionController::class, 'BQ'])->name('BQ');
-Route::post('/Add/BallotQuestion', [\App\Http\Controllers\BallotQuestionController::class, 'AddBQ'])->name('AddBQ');
-Route::get('/View/BallotQuestion', [\App\Http\Controllers\BallotQuestionController::class, 'ViewBQ'])->name('ViewBQ');
-Route::get('/Edit/BallotQuestion/{id?}', [\App\Http\Controllers\BallotQuestionController::class, 'ViewUpdateBQ'])->name('ViewUpdateBQ');
-Route::post('/Update/BallotQuestion', [\App\Http\Controllers\BallotQuestionController::class, 'UpdateBQ'])->name('UpdateBQ');
-Route::get('/Delete/BallotQuestion/{id?}', [\App\Http\Controllers\BallotQuestionController::class, 'DeleteBQ'])->name('DeleteBQ');
+Route::get('dashboard/BallotQuestion', [\App\Http\Controllers\BallotQuestionController::class, 'BQ'])->name('BQ');
+Route::post('dashboard/Add/BallotQuestion', [\App\Http\Controllers\BallotQuestionController::class, 'AddBQ'])->name('AddBQ');
+Route::get('dashboard/View/BallotQuestion', [\App\Http\Controllers\BallotQuestionController::class, 'ViewBQ'])->name('ViewBQ');
+Route::get('dashboard/Edit/BallotQuestion/{id?}', [\App\Http\Controllers\BallotQuestionController::class, 'ViewUpdateBQ'])->name('ViewUpdateBQ');
+Route::post('dashboard/Update/BallotQuestion', [\App\Http\Controllers\BallotQuestionController::class, 'UpdateBQ'])->name('UpdateBQ');
+Route::get('dashboard/Delete/BallotQuestion/{id?}', [\App\Http\Controllers\BallotQuestionController::class, 'DeleteBQ'])->name('DeleteBQ');
 
 
-Route::post('/Update/BallotQuestion', [\App\Http\Controllers\BallotOptionController::class, 'UpdateBO'])->name('UpdateBO');
+Route::post('dashboard/Update/BallotQuestion', [\App\Http\Controllers\BallotOptionController::class, 'UpdateBO'])->name('UpdateBO');
 
 Route::get('dashboard/Election', [\App\Http\Controllers\ElectionController::class, 'ElectionIndex'])->name('Election.Index');
 Route::get('dashboard/Add/Election', [\App\Http\Controllers\ElectionController::class, 'AddElection'])->name('AddElection');
