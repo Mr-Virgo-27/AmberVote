486c1c30d2d5600f7089e5ba88103f2a3fcea1c8
KevornC <kevorn.callum16@gmail.com>
2021-10-11 23:06:50 -0500

    commit

--- routes/kevorn.php
+++ routes/kevorn.php
@@ -6,18 +6,18 @@ use App\Http\Controllers\VoterController;
 
 //Adding Voters Routes
 
-Route::get('/Add/Election/Voter', [VoterController::class, 'index'])->name('AddVoterIndex');
+Route::get('dashboard/Add/Election/Voter', [VoterController::class, 'index'])->name('AddVoterIndex');
 
-Route::post('/Add/Election/Voter', [VoterController::class, 'addvoter'])->name('AddVoterIndex');
+Route::post('dashboard/Add/Election/Voter', [VoterController::class, 'addvoter'])->name('AddVoterIndex');
 
 //View Voters Info Routes
-Route::get('/View/Election/Voters', [VoterController::class, 'view'])->name('ViewVoterIndex');
+Route::get('dashboard/View/Election/Voters', [VoterController::class, 'view'])->name('ViewVoterIndex');
 
 //Edit Voters Info Routes
-Route::get('/Edit/Election/Voter{id}', [VoterController::class, 'edit'])->name('EditVoterIndex');
+Route::get('dashboard/Edit/Election/Voter{id}', [VoterController::class, 'edit'])->name('EditVoterIndex');
 
-Route::post('/Edit/Election/Voter', [VoterController::class, 'update'])->name('EditVoterIndex');
+Route::post('dashboard/Edit/Election/Voter', [VoterController::class, 'update'])->name('EditVoterIndex');
 
 //Delete Voter
 
-Route::get('/Delete/Election/Voter{id}', [VoterController::class, 'delete'])->name('DeleteVoterIndex');
+Route::get('dashboard/Delete/Election/Voter{id}', [VoterController::class, 'delete'])->name('DeleteVoterIndex');
