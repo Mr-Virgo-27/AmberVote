4b4cf1737986617925902b60aa3f91e5061ac9a0
Richard Wilson <richardmwilson191@gmail.com>
2021-10-11 19:12:06 -0500

    Connected elections with voters allowing users to add voters and send out notifications

--- app/Http/Controllers/ElectionController.php
+++ app/Http/Controllers/ElectionController.php
@@ -21,8 +21,15 @@ class ElectionController extends Controller
 
     public function ElectionStore(Request $req)
     {
+        $req->validate([
+            'election_nm' => 'required',
+            'start' => 'required|date',
+            'end' => 'required|date',
+            'desc' => 'required'
+        ]);
+
         Election::create([
-            'user_id' => Auth::user(),
+            'user_id' => Auth::id(),
             'election_nm' => $req->election_nm,
             'start_date' => $req->start,
             'end_date' => $req->end,
@@ -36,5 +43,37 @@ class ElectionController extends Controller
     {
         $election = Election::find($id);
         return view('election.show', compact('election'));
+    }
+
+    public function edit($id)
+    {
+        $election = Election::find($id);
+        return view('election.edit', compact('election'));
+    }
+
+    public function update(Request $req, $id)
+    {
+        $req->validate([
+            'election_nm' => 'required',
+            'start' => 'required|date',
+            'end' => 'required|date',
+            'desc' => 'required'
+        ]);
+
+        Election::find($id)->update([
+            'user_id' => Auth::id(),
+            'election_nm' => $req->election_nm,
+            'start_date' => $req->start,
+            'end_date' => $req->end,
+            'desc' => $req->desc
+        ]);
+
+        return redirect()->route('election.show', $id);
+    }
+
+    public function destroy($id)
+    {
+        Election::destroy($id);
+        return redirect()->route('Election.Index');
     }
 }
