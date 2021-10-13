64ee33b9d2c26f4c16d2395f4b7d466094092ca9
Richard Wilson <richardmwilson191@gmail.com>
2021-10-13 09:42:59 -0500

    implementing election notification system, import voters by csv

--- database/migrations/2021_10_08_000345_create_elections_table.php
+++ database/migrations/2021_10_08_000345_create_elections_table.php
@@ -22,6 +22,7 @@ class CreateElectionsTable extends Migration
             $table->string('desc');
             $table->string('status')->default('active');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- resources/views/election/show.blade.php
+++ resources/views/election/show.blade.php
@@ -1,41 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="w-full flex flex-col ">
+    <div class="flex flex-col">
         <div class="w-full my-4">
             <a class="p-2 px-5 bg-blue-600 text-gray-100 text-lg rounded-lg focus:border-4 border-blue-300 hover:bg-blue-400 hover:text-gray-100"
                 href="{{ route('ballots', $election->id) }}">Create
                 Ballot</a>
             <a class="p-2 px-5 bg-blue-600 text-gray-100 text-lg rounded-lg focus:border-4 border-blue-300 hover:bg-blue-400 hover:text-gray-100"
-                href="{{ route('voter.create') }}">Add Voters</a>
+                href="{{ route('voter.create', $election->id) }}">Add Voters</a>
             <a class="p-2 px-5 bg-blue-600 text-gray-100 text-lg rounded-lg focus:border-4 border-blue-300 hover:bg-blue-400 hover:text-gray-100"
                 href="{{ route('ViewVoterIndex') }}">View Voters</a>
         </div>
-        <div class="m-auto bg-white shadow-md rounded px-8 pt-6 pb-8 m-4">
-            <ul>
-                <li class="text-lg font-bold my-4">{{ $election->election_nm }}</li>
-                <li class="my-2">
-                    <span class="font-semibold">Start Date:
-                    </span>{{ $election->start_date }}
-                </li>
-                <li class="my-2">
-                    <span class="font-semibold">End Date:
-                    </span>{{ $election->end_date }}
-                </li>
-                <li class="my-2">
-                    <span class="font-semibold">Description:
-                    </span>{{ $election->desc }}
-                </li>
-                <li class="my-2">
-                    <span class="font-semibold">Election Status:
-                    </span>{{ $election->status }}
-                </li>
-            </ul>
+        <div class="flex my-12 m-auto">
+            <div class="flex mr-12">
+                <div class="bg-white shadow-md rounded px-8 pt-6 pb-8">
+                    <ul>
+                        <li class="text-lg font-bold my-4">{{ $election->election_nm }}</li>
+                        <li class="my-2">
+                            <span class="font-semibold">Start Date:
+                            </span>{{ $election->start_date }}
+                        </li>
+                        <li class="my-2">
+                            <span class="font-semibold">End Date:
+                            </span>{{ $election->end_date }}
+                        </li>
+                        <li class="my-2">
+                            <span class="font-semibold">Description:
+                            </span>{{ $election->desc }}
+                        </li>
+                        <li class="my-2">
+                            <span class="font-semibold">Election Status:
+                            </span>{{ $election->status }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div>
+                <div class="bg-white shadow-md rounded px-8 pt-6 pb-8">
+                    <ul>
+                        <li class="text-lg font-bold my-4">Ballot</li>
+                        <li class="my-2">
+                            <span class="font-semibold">Ballot Type:
+                            </span>{{ $ballot->ballot_type }}
+                        </li>
+                        <li class="my-2">
+                            <span class="font-semibold">Description:
+                            </span>{{ $ballot->desc }}
+                        </li>
+                        <li class="my-2">
+                            <span class="font-semibold">Date Created:
+                            </span>{{ $ballot->created_at }}
+                        </li>
+                    </ul>
+                    <div class="mt-8">
+                        <a class="p-2 px-5 bg-blue-600 text-gray-100 text-lg rounded-lg focus:border-4 border-blue-300 hover:bg-blue-400 hover:text-gray-100"
+                            href="{{ route('ballots', $ballot->id) }}">Add Questions</a>
+                    </div>
+                </div>
+            </div>
+
         </div>
         <div>
             <a class="
-                    text-lg font-semibold text-red-500" href="{{ url()->previous() }}"
-                type="submit">
+                text-lg font-semibold text-red-500" href="{{ url()->previous() }}" type="submit">
                 {{ '<<< Back' }}
             </a>
         </div>
